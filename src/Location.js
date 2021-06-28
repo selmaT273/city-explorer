15cf4833c9e0e34d194f37a53d8679bd966bc0f1
Stacey Teltser <stacey.teltser@gmail.com>
2021-06-28 14:51:57 -0500

    removed latitude and longitude from state because no need to pull it out and set state of it in the location component

--- src/Location.js
+++ src/Location.js
@@ -1,22 +1,15 @@
 import React from 'react';
 
 class Location extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            latitude: this.props.data.lat,
-            longitude: this.props.data.lon,
-        }
-    }
 
     render(){
         return(
             <>
             <h2>Location Information:</h2>
             <h3>{this.props.data.display_name}</h3>
-            <h4>{this.state.latitude}</h4>
-            <h4>{this.state.longitude}</h4>
-            <img src={`https://maps.locationiq.com/v3/staticmap?key=${this.props.key}&center=${this.state.latitude},${this.state.longitude}`} alt='map' />
+            <h4>{this.props.data.lat}</h4>
+            <h4>{this.props.data.lon}</h4>
+            <img src={`https://maps.locationiq.com/v3/staticmap?key=${this.props.key}&center=${this.props.lat},${this.props.lon}`} alt='map' />
             </>
         );
     }
