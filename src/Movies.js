239b75f58960e13e90e9755ba0af05183f7b32e6
Stacey Teltser <stacey.teltser@gmail.com>
2021-07-13 18:18:10 -0500

    componentized each individual movie

--- src/Movies.js
+++ src/Movies.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Movies.css';
-import Image from 'react-bootstrap/Image';
-
+import Movie from './Movie';
 class Movies extends React.Component {
   render(){
     return(
@@ -10,18 +9,7 @@ class Movies extends React.Component {
           <>
             <h3>Movies:</h3>
             {this.props.movies.map(movie =>
-              (<ul className="movie-list" key={movie.title}>
-                <li><h4>{movie.title}</h4></li>
-                <li><i>Released: {movie.released_on}</i></li>
-                {movie.image_url === null
-                  ? '*No Image Available*'
-                  : <li><Image src={`https://image.tmdb.org/t/p/w300/${movie.image_url}`}/></li>
-                }
-                <li>{movie.overview}</li>
-                <li><b>Avg Votes:</b> {movie.average_votes}</li>
-                <li><b>Total Votes:</b> {movie.total_votes}</li>
-                <li><b>Popularity Rating:</b> {movie.popularity}</li>
-              </ul>)
+              (<Movie key={movie.title} movie={movie} />)
             )}
           </>
         }
