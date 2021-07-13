7206b7e1ba7f03072282c394715611e64cb96ae7
Stacey Teltser <stacey.teltser@gmail.com>
2021-07-13 12:03:54 -0500

    rendering movie data onto page

--- src/Movies.js
+++ src/Movies.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './Movies.css';
+import Image from 'react-bootstrap/Image';
 
 class Movies extends React.Component {
   render(){
@@ -8,9 +10,18 @@ class Movies extends React.Component {
           <>
             <h3>Movies:</h3>
             {this.props.movies.map(movie =>
-              (<p key={movie.title}>
-                Title: {movie.title}
-              </p>)
+              (<ul className="movie-list" key={movie.title}>
+                <li><h4>{movie.title}</h4></li>
+                <li><i>Released: {movie.released_on}</i></li>
+                {movie.image_url === null
+                  ? '*No Image Available*'
+                  : <li><Image src={`https://image.tmdb.org/t/p/w300/${movie.image_url}`}/></li>
+                }
+                <li>{movie.overview}</li>
+                <li><b>Avg Votes:</b> {movie.average_votes}</li>
+                <li><b>Total Votes:</b> {movie.total_votes}</li>
+                <li><b>Popularity Rating:</b> {movie.popularity}</li>
+              </ul>)
             )}
           </>
         }
